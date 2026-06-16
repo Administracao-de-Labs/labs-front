a05e92b74b423aa65ad64f9894859bb8a235de82
annandajacobs <annandajacobs@gmail.com>
2026-06-16 20:37:22 -0300

    cria endpoint de listClients

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,11 @@
 // centraliza as chamadas http
+BASE_URL = 'http://localhost:4000';
+
+export async function listClients() {
+    const response = await fetch(`${BASE_URL}/api/v1/clients`);
+    if (!response.ok) {
+        throw new Error('Erro ao buscar clientes');
+    }
+    return response.json();
+}
+
